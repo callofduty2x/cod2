f3c0732ed07cd93b5a7d55934eefd6f00ca43a82
Prawy <70172890+PrawyCoD1@users.noreply.github.com>
2024-02-18 17:41:04 +0100

    Update cm_tracebox.cpp

--- qcommon/cm_tracebox.cpp
+++ qcommon/cm_tracebox.cpp
@@ -1,9 +1,22 @@
+#include "qcommon.h"
+#include "sys_thread.h"
+
 void __cdecl CM_CalcTraceEntents(struct TraceExtents * extents)
 {	UNIMPLEMENTED();
 }
 
-int __cdecl CM_TraceBox(struct TraceExtents const * extents, float const * const mins, float const * const maxs, float fraction)
-{	UNIMPLEMENTED();
-}
+qboolean CM_TraceBox(TraceExtents *extents, const float *mins, const float *maxs, float fraction)
+{
+	float lfraction = fraction;
 
-int marker_cm_tracebox;
+	if(CM_TraceBoxInternal(extents, mins, -1.0, &lfraction) == qtrue)
+	{
+		return qtrue;
+	}
+	if(CM_TraceBoxInternal(extents, maxs, 1.0, &lfraction) == qtrue)
+	{
+		return qtrue;
+	}
+
+	return qfalse;
+}

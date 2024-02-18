30e3b742c79ae3afc8293fc35ecf0dc7259a41d0
Prawy <70172890+PrawyCoD1@users.noreply.github.com>
2024-02-18 14:12:04 +0100

    Update sv_voice_mp.cpp

--- server_mp/sv_voice_mp.cpp
+++ server_mp/sv_voice_mp.cpp
@@ -1,28 +1,130 @@
-void __cdecl SV_QueueVoicePacket(int talkerNum, int clientNum, unsigned char * voicePacket)
-{	UNIMPLEMENTED();
+#include "../qcommon/qcommon.h"
+#include "../qcommon/netchan.h"
+
+extern dvar_t *sv_voice;
+
+void SV_QueueVoicePacket(int talkerNum, int clientNum, VoicePacket_t *voicePacket)
+{
+	client_s *client;
+
+	client = &svs.clients[clientNum];
+
+	if ( client->unsentVoiceData < 40 )
+	{
+		client->voicedata[client->unsentVoiceData].dataLen = voicePacket->dataLen;
+		memcpy(client->voicedata[client->unsentVoiceData].data, voicePacket->data, voicePacket->dataLen);
+		client->voicedata[client->unsentVoiceData++].num = talkerNum;
+	}
 }
 
-bool __cdecl SV_ClientHasClientMuted(int listener, int talker)
-{	UNIMPLEMENTED();
+bool SV_ClientHasClientMuted(int listener, int talker)
+{
+	return svs.clients[listener].mutedClients[talker];
 }
 
-void __cdecl SV_PreGameUserVoice(struct client_s * cl, struct msg_t * msg)
-{	UNIMPLEMENTED();
+void SV_PreGameUserVoice(client_s *cl, msg_t *msg)
+{
+	int talker;
+	int listener;
+	int packet;
+	int packetCount;
+	VoicePacket_t voicePacket;
+
+	if ( sv_voice->current.boolean )
+	{
+		talker = cl - svs.clients;
+		packetCount = MSG_ReadByte(msg);
+
+		for ( packet = 0; packet < packetCount; ++packet )
+		{
+			voicePacket.dataLen = MSG_ReadShort(msg);
+
+			if ( voicePacket.dataLen <= 0 || voicePacket.dataLen > 256 )
+			{
+				Com_Printf("Received invalid voice packet of size %i from %s\n", voicePacket.dataLen, cl->name);
+				return;
+			}
+
+			MSG_ReadData(msg, voicePacket.data, voicePacket.dataLen);
+
+			for ( listener = 0; listener <= 63; ++listener )
+			{
+				if ( listener != talker
+				        && svs.clients[listener].state > CS_ZOMBIE
+				        && !SV_ClientHasClientMuted(listener, talker)
+				        && SV_ClientWantsVoiceData(listener) )
+				{
+					SV_QueueVoicePacket(talker, listener, &voicePacket);
+				}
+			}
+		}
+	}
 }
 
-void __cdecl SV_UserVoice(struct client_s * cl, struct msg_t * msg)
-{	UNIMPLEMENTED();
+void SV_UserVoice(client_s *cl, msg_t *msg)
+{
+	int packet;
+	int packetCount;
+	VoicePacket_t voicePacket;
+
+	if ( sv_voice->current.boolean )
+	{
+		packetCount = MSG_ReadByte(msg);
+
+		for ( packet = 0; packet < packetCount; ++packet )
+		{
+			voicePacket.dataLen = MSG_ReadByte(msg);
+
+			if ( voicePacket.dataLen <= 0 || voicePacket.dataLen > 256 )
+			{
+				Com_Printf("Received invalid voice packet of size %i from %s\n", voicePacket.dataLen, cl->name);
+				return;
+			}
+
+			MSG_ReadData(msg, voicePacket.data, voicePacket.dataLen);
+			G_BroadcastVoice(cl->gentity, &voicePacket);
+		}
+	}
 }
 
-void __cdecl SV_SendClientVoiceData(struct client_s * client)
-{	UNIMPLEMENTED();
+void SV_SendClientVoiceData(client_s *client)
+{
+	byte buf[MAX_LARGE_MSGLEN];
+	msg_t msg;
+
+	if ( client->state == CS_ACTIVE && client->unsentVoiceData )
+	{
+		MSG_Init(&msg, buf, sizeof(buf));
+		MSG_WriteString(&msg, "v");
+		SV_WriteClientVoiceData(client, &msg);
+
+		if ( msg.overflowed )
+		{
+			Com_Printf("WARNING: voice msg overflowed for %s\n", client->name);
+		}
+		else
+		{
+			NET_OutOfBandVoiceData(NS_SERVER, client->netchan.remoteAddress, msg.data, msg.cursize);
+			client->unsentVoiceData = 0;
+		}
+	}
 }
 
-void __cdecl SV_WriteVoiceDataToClient(struct client_s *,struct msg_t *)
-{	UNIMPLEMENTED();
+void SV_WriteVoiceDataToClient(client_s *client, msg_t *msg)
+{
+  int i;
+
+	MSG_WriteByte(msg, client->unsentVoiceData);
+
+	for ( i = 0; i < client->unsentVoiceData; ++i )
+	{
+		MSG_WriteByte(msg, client->voicedata[i].num);
+		MSG_WriteByte(msg, client->voicedata[i].dataLen);
+		MSG_WriteData(msg, client->voicedata[i].data, client->voicedata[i].dataLen);
+	}
 }
 
-SV_ClientWantsVoiceData(int clientNum)
-{	UNIMPLEMENTED();
+bool SV_ClientWantsVoiceData(unsigned int clientNum)
+{
+	return svs.clients[clientNum].hasVoip;
 }
-
